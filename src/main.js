70f6aab73dd2087bb4f61af85554ff250af763d8
emihle <mihlekcebisa@gmail.com>
2022-02-17 13:20:00 +0200

    createdafrontend

--- src/main.js
+++ src/main.js
@@ -2,5 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import VeeValidate from 'vee-validate';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from './plugins/font-awesome'
+import {
+  faHome,
+  faUser,
+  faUserPlus,
+  faSignInAlt,
+  faSignOutAlt
+} from '@fortawesome/free-solid-svg-icons';
+library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
+// Vue.config.productionTip = false;
+// Vue.component('font-awesome-icon', FontAwesomeIcon);
+createApp(App).use(store).use(router)  
+// .component("font-awesome-icon", FontAwesomeIcon)
+.mount('#app')
